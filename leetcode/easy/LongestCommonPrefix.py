9fd8a41a868cb0dc4bc5efef77337ef11e369132
chenjunqian <chenjunqian0810@foxmail.com>
2019-06-18 00:37:54 +0800

    添加注释

--- leetcode/easy/LongestCommonPrefix.py
+++ leetcode/easy/LongestCommonPrefix.py
@@ -1,3 +1,8 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+# Example 1:
+# Input: ["flower","flow","flight"]
+# Output: "fl"
 class Solution:
     def longestCommonPrefix(self, strs):
         """
--- leetcode/easy/PalindromeNumber.py
+++ leetcode/easy/PalindromeNumber.py
@@ -1,3 +1,8 @@
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+# Note: For the purpose of this problem, we define empty string as valid palindrome.
+# Example 
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
 class Solution:
     def isPalindrome(self, x):
         """
